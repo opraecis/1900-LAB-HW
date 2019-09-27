6510a9d1f08a067d1ae36395d3d0b7946436fefd
Huy Tran <53249174+opraecis@users.noreply.github.com>
2019-09-27 09:24:42 -0500

    Add files via upload

---
+++ AirportParking.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class AirportParking{
+	public static void main(String[] args){
+		Scanner scnr = new Scanner(System.in);
+
+		//Taking Input
+		System.out.print("Enter number of parking minutes: ");
+		int minInput = scnr.nextInt();
+
+		//Setting variables to default values
+		int numDays = minInput / 1440;
+		int totalMins = minInput % 1440;
+		int num30Mins = 0;
+		int numMins = 0;
+		int amountCharged = 24 * numDays;
+
+		if  (minInput < 0){
+			System.out.println("Error - Number out of bound.");
+		}
+		else {
+			num30Mins = totalMins / 30;
+			numMins = totalMins % 30;
+
+			if (numMins > 0){
+				num30Mins++;
+			} 
+
+			switch (num30Mins){
+				case 0:
+				case 1:
+					break;
+				case 2:
+					amountCharged += 2;
+					break;
+				default:
+					int over1HCharged = 2 + (1 * (num30Mins-2));
+					if (over1HCharged >= 24) { 
+						over1HCharged = 24;
+					}
+					amountCharged += over1HCharged;
+					break;
+			}
+			System.out.println("Total charged: $" + amountCharged);
+
+		}
+		
+	}
+}
